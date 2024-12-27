4dbe178780968295c8a640dd25354c39af12733d
Red Daly <reddaly@gmail.com>
2024-12-27 10:57:56 -0800

    Improve support for writing generated files to source directory. (#40)
    1. Add a macro that wraps `go_proto_library` that creates a bazel runnable command for generating .pb.go files.
     Add a `gazelle:map_kind go_proto_library go_proto_library //bazel:go_proto_library.bzl` directive to use this macro instead of the default go_proto_library.
    
    2. Add `regenerate-pb-go-files.sh` for regenerating the .pb.go files and update readme to point to that.
    
    4. Delete `iotcore` references since that GCP service has been turned down, and in general I'd like to move the cloud-related functionality out of this repo for simplicity.

---
+++ proto/README.md
@@ -0,0 +1,7 @@
+## Keeping generate .pb.go files up to date
+
+Run the following command:
+
+```shell
+./tools/regnerate-pb-go-files.sh
+```
---
+++ tools/regenerate-pb-go-files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# cd to the root of the git repo
+cd "$(git rev-parse --show-toplevel)"
+
+# There should be one command for each result of
+# bazel query 'kind("go_proto_library", //...)'
+
+bazel run //proto/chiltrix:chiltrix_go_proto_write_source_files
+bazel run //proto/command_queue:command_queue_go_proto_write_source_files
+bazel run //proto/controller:controller_go_proto_write_source_files
+bazel run //proto/fancoil:fancoil_go_proto_write_source_files
